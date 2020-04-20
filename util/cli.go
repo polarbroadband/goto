f621dfe808e3bd0ec45c6c9067418123c64755cd
Kelvin Wang <kelvin.wang@telus.com>
2020-04-19 22:23:10 -0400

    add GetInput

--- util/cli.go
+++ util/cli.go
@@ -14,24 +14,33 @@ import (
 /* ****************************************
 cli utility functions
 **************************************** */
+// GetInput display prompt and return trimed input string
+// return empty string if input not valid
+func GetInput(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(prompt + ": ")
+	s, err := reader.ReadString('\n')
+	if err != nil {
+		log.WithError(err).Warnf("erroneous input of %s", prompt)
+		return ""
+	}
+	return strings.TrimSpace(s)
+}
 
 // GetCred prompt for entering username and password
 // return empty strings if input not valid
 // no screen echo for entering password
-func GetCred() (uid, pwd string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Username: ")
-	name, err := reader.ReadString('\n')
-	if err != nil {
-		log.WithError(err).Warn("erroneous username input")
+func GetCred() (string, string) {
+	uid := GetInput("Username")
+	if uid == "" {
 		return "", ""
 	}
 	fmt.Print("Password: ")
 	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
 	if err != nil {
-		log.WithError(err).Warn("erroneous password input")
+		log.WithError(err).Warn("erroneous input of password")
 		return "", ""
 	}
 	fmt.Println()
-	return strings.TrimSpace(name), strings.TrimSpace(string(bytePassword))
+	return uid, strings.TrimSpace(string(bytePassword))
 }
